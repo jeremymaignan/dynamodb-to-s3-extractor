1a23abef9fe6f9af5b47d38f87b6e4552f2bcca5
jeremymaignan <jeremy.maignan@gmail.com>
2018-08-26 20:31:09 +0200

    add params in commmand line

--- export_dynamodb_to_s3.py
+++ export_dynamodb_to_s3.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3.7
 
-from utils.ConfManager import get_conf
+from utils.ConfManager import Configuration
 from utils.Dynamo import Dynamo
 from utils.FileManager import FileManager
 from utils.S3 import S3
@@ -9,35 +9,46 @@ from datetime import datetime
 from time import strftime
 from tqdm import tqdm
 import os
+import sys
 
 if __name__ == '__main__':
+    configuration = Configuration(sys.argv[1:])
     os.system("mkdir -p tmp")
     print("[CONF] Tablename: {}  AWS region: {}  File size: {}  Output format: {}  Bucket name: {}  Path on S3: {}  Delete Items: {}".format(
-        get_conf("tablename"), 
-        get_conf("aws_region"), 
-        get_conf("limit_per_file"),
-        get_conf("output_format"),
-        get_conf("bucket_name"), 
-        get_conf("bucket_path"),
-        get_conf("delete_items"),
-        ))
+        configuration.conf["tablename"], 
+        configuration.conf["aws_region"], 
+        configuration.conf["limit_per_file"],
+        configuration.conf["output_format"],
+        configuration.conf["bucket_name"],
+        configuration.conf["bucket_path"],
+        configuration.conf["delete_items"],
+    ))
     # Get Items from DB
-    db = Dynamo(get_conf("tablename"), get_conf("aws_region"))
-    items = db.fetch_all_items(get_conf("limit_per_file"))
+    db = Dynamo(
+        configuration.conf["tablename"], 
+        configuration.conf["aws_region"]
+    )
+    items = db.fetch_all_items(configuration.conf["limit_per_file"])
     print("[INFO] {} items found".format(len(items)))
                                                                                                                                     
     # Write file
     fm = FileManager()
-    filename =  "{}_{}.{}".format(get_conf("filename"), datetime.now().strftime("%Y-%m-%d_%H:%M:%S"), get_conf("output_format"))
-    if "json" == get_conf("output_format"):
+    filename =  "{}_{}.{}".format(
+        configuration.conf["filename"],
+        datetime.now().strftime("%Y-%m-%d_%H:%M:%S"),
+        configuration.conf["output_format"])
+    if "json" == configuration.conf["output_format"]:
         fm.write_json(items, filename)
     else:
         fm.write_csv(items, filename)
 
     # Upload file on S3
     s3 = S3()
-    if s3.upload_s3(get_conf("bucket_name"), get_conf("bucket_path"), filename):
-        if get_conf("delete_items"):
+    if s3.upload_s3(
+        configuration.conf["bucket_name"], 
+        configuration.conf["bucket_path"], 
+        filename):
+        if configuration.conf["delete_items"]:
             # Delete items in Dynamo
             db.delete_items(items)
         else:
--- utils/ConfManager.py
+++ utils/ConfManager.py
@@ -1,12 +1,66 @@
 import yaml
 import os
+import sys, getopt
 
-# Get configuration from env
-# Use default conf if the configuration is not set in env
-def get_conf(key):
-    try:
-        return os.environ[key.upper()]
-    except:
+class Configuration():
+
+    def __init__(self, argv):
+        self.conf = self.get_conf_file()
+        self.get_conf_from_env()
+        self.get_conf_from_params(argv)
+        self.check_types()
+
+    def get_conf_file(self):
         with open('config.yaml') as json_data_file:
             conf = yaml.load(json_data_file)
-            return conf[key]
+            return conf 
+
+    def get_conf_from_env(self):
+        for key in self.conf.keys():
+            try:
+                self.conf[key] = os.environ[key.upper()]
+            except:
+                pass
+
+    def get_conf_from_params(self, argv):
+        try:
+            opts, args = getopt.getopt(argv, "o:f:t:a:l:b:p:d:")
+        except getopt.GetoptError:
+            print('test.py -o <outputfile> -f <format> -t <tablename> -a <aws_region> -l <limit> -b <bucket_name> -p <bucket_path> -d <delete>')
+            sys.exit(2)
+        for opt, arg in opts:
+            if opt in ('-h', "--help"):
+                print('test.py -o <outputfile> -f <format> -t <tablename> -a <aws_region> -l <limit> -b <bucket_name> -p <bucket_path> -d <delete>')
+                sys.exit()
+            elif opt in ("-o", "--outputfile"):
+                self.conf["filename"] = arg
+            elif opt in ("-f", "--format"):
+                self.conf["output_format"] = arg
+            elif opt in ("-t", "--tablename"):
+                self.conf["tablename"] = arg
+            elif opt in ("-a", "--aws_region"):
+                self.conf["aws_region"] = arg
+            elif opt in ("-l", "--limit"):
+                self.conf["limit_per_file"] = arg
+            elif opt in ("-b", "--bucket_name"):
+                self.conf["bucket_name"] = arg
+            elif opt in ("-p", "--bucket_path"):
+                self.conf["bucket_path"] = arg
+            elif opt in ("-d", "--delete"):
+                self.conf["delete_items"] = arg
+
+    def check_types(self):
+        if str == type(self.conf["delete_items"]):
+            if "true" == self.conf["delete_items"].lower():
+                self.conf["delete_items"] = True
+            elif "false" == self.conf["delete_items"].lower():
+                self.conf["delete_items"] = False
+            else:
+                print("Param Error. delete_items must be a boolean")
+                sys.exit(2)
+        if str == type(self.conf["limit_per_file"]):
+            try:
+                self.conf["limit_per_file"] = int(self.conf["limit_per_file"])
+            except:
+                print("Param Error. limit_per_file must be an integer")
+                sys.exit(2)
