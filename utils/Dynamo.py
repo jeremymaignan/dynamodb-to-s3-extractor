6ef7ceb1b15bc39f0f6b588906195ce9c90d9235
jeremymaignan <jeremy.maignan@gmail.com>
2018-08-26 21:30:10 +0200

    add dynamic loading of the indexes, to delete items

--- utils/Dynamo.py
+++ utils/Dynamo.py
@@ -6,8 +6,24 @@ class Dynamo():
     def __init__(self, tablename, aws_region):
         dynamodb = boto3.resource('dynamodb', aws_region)
         self.table = dynamodb.Table(tablename)
+        self.client = boto3.client('dynamodb', aws_region)
+        self.tablename = tablename
         print("[INFO] Connected to {}".format(tablename))
 
+    def get_index(self):
+        hash_ = ""
+        range_ = ""
+        indexes = self.client.describe_table(
+            TableName=self.tablename
+        )["Table"]["KeySchema"]
+        for index in indexes:
+            if "HASH" == index["KeyType"]:
+                hash_ = index["AttributeName"]
+            if "RANGE" == index["KeyType"]:
+                range_ = index["AttributeName"]
+        print("[INFO] Range: {} Hash: {}".format(hash_, range_))
+        return hash_, range_
+        
     # Fetch $limit items from db
     # Loop and use an offset to fetch enough items
     def fetch_all_items(self, limit=0):
@@ -25,9 +41,12 @@ class Dynamo():
     # Delete items one by one
     def delete_items(self, items):
         print("[INFO] Deleting items")
+        hash_, range_ = self.get_index()
         for item in tqdm(items):
-            self.table.delete_item(Key={
-                 "email": item["email"],
-                 "id": item["id"]
-            })
+            key = {}
+            if None != hash_:
+                key[hash_] = item[hash_]
+            if None != range_:
+                key[range_] = item[range_]
+            self.table.delete_item(Key=key)
         print("[INFO] {} Items deletes".format(len(items)))
